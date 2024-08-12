791a2709eaf619a1ae77243e8574041a705be44a
yongqi8899 <1243994415@qq.com>
2024-08-12 10:43:50 +0200

    add BE init

---
+++ index.js
@@ -0,0 +1,19 @@
+import "./db/index.js";
+import express from "express";
+import cors from "cors";
+
+import errorHandler from "./middlewares/errorHandler.js";
+import dbInit from "./db/index.js";
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors({ origin: process.env.SPA_ORIGIN, credentials: true }));
+app.use(express.json());
+
+app.use("*", (req, res) => res.status(404).json({ error: "Not found" }));
+app.use(errorHandler);
+
+dbInit()
+
+app.listen(port, () => console.log(`Server listening on port : ${port}`));
---
+++ notes.md
@@ -0,0 +1,5 @@
+Mongoose+starter
+
+PxDCnDyohTa3C2FN
+
+MONGO_URI = mongodb+srv://zhangyongqi7:PxDCnDyohTa3C2FN@starter.c7yka.mongodb.net/?retryWrites=true&w=majority&appName=starter
